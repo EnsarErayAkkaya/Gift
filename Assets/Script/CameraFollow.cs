18ce8526c91e00d89a303d3347262e7fdf682af4
Ensar Eray Akkaya <33553090+EnsarErayAkkaya@users.noreply.github.com>
2020-12-21 23:19:26 +0300

    Initial

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Camera camera;
+    CharacterMovement player;
+    public float followDistance;
+    public float cameraLerpSpeed;
+    public Vector3 cameraOffset;
+
+    private void Start()
+    {
+        player = FindObjectOfType<CharacterMovement>();
+    }
+    void LateUpdate()
+    {
+        if (RangeCheck())
+        {
+            if (camera.transform.position != new Vector3(player.transform.position.x, player.transform.position.y, -1))
+            {
+                camera.transform.position = Vector3.MoveTowards(camera.transform.position, player.transform.position + cameraOffset
+                    , cameraLerpSpeed * Time.deltaTime);
+            }
+        }   
+    }
+    bool RangeCheck()
+    {
+        return Vector3.Distance(player.transform.position + cameraOffset, camera.transform.position) > followDistance;
+    }
+}
---
+++ Assets/Script/CharacterMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float speed;
+    public bool canMove;
+
+    [Header("Jump")]
+    public bool canJump;
+    public float jumpSpeed;
+    public float fallSpeed;
+    public float jumpHeight;
+
+    private float jumpTargetHeight;
+    private bool startJump;
+    private bool jumpRising;
+    private bool jumpFalling;
+
+    /// Private ///
+
+    private Vector2 moveVector;
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            moveVector = Vector2.right * speed;
+        }
+
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            startJump = true;
+        }
+
+        if (startJump)
+        {
+            jumpTargetHeight = transform.position.y + jumpHeight;
+            jumpRising = true;
+            startJump = false;
+        }
+        if (jumpRising)
+        {
+            moveVector.y = Vector2.up.y * jumpSpeed;
+            if(transform.position.y >= jumpTargetHeight)
+            {
+                jumpRising = false;
+                jumpFalling = true;
+            }
+        }
+        if (jumpFalling)
+        {
+            moveVector.y = -Vector2.up.y * fallSpeed;
+        }
+
+        transform.position += (Vector3)moveVector * Time.deltaTime;
+        
+        if(transform.position.y < 0)
+        {
+            jumpFalling = false;
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+    }
+}
